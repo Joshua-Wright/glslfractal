e15f7c4dee9d14e1955a3a0d3cf6ac0884426126
Josh Wright <madewithlinux.josh@gmail.com>
2016-07-21 23:05:14 -0500

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# this file is intentionally not executable, because it is a little bit dangerous.
 
-git checkout gh-pages
+git branch -D gh-pages
+git checkout --orphan gh-pages
 
 webpack -p
 git add build/bundle.js
 git commit -m "deploy"
-git push origin gh-pages
+git push -f origin gh-pages
 
 git checkout master
